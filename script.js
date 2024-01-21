191d97d65e0f1aa8e0ac00fb64df3dbdfdf9b4a1
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2024-01-21 12:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 //your JS code here. If required.
+let arr = [1,2,3,4];
+
+function promise1(arr){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			 resolve(arr);
+		},3000)
+	})
+}
+
+function promise2(arr){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			let data = arr.filter((e)=> e%2 === 0);
+			resolve(data);
+			// let output = document.getElementById("output");
+			// output.innerContext = data;
+		},1000)
+	})
+}
+
+function promise3(data){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			let ans = data.map((e)=> e*2);
+			 resolve(ans);
+			// let output = document.getElementById("output");
+			// output.innerContext = ans;
+		},2000)
+	})
+}
+
+promise1(arr).then((data)=>{
+	console.log(data);
+	return promise2(data);
+}).then((data)=>{
+	console.log(data);
+	let output = document.getElementById("output");
+			output.textContent = data;
+	return promise3(data);
+}).then((data)=>{
+	let output = document.getElementById("output");
+			output.textContent = data;
+	console.log(data);
+}).catch((err)=>{
+	console.log(err);
+})
